8b77a8fd76667d51e2fcdcef1ee2a2a6fd01f38d
Furkan Demirbas <m.furkan0508@gmail.com>
2025-12-22 16:41:15 +0100

    feat: bootstrap spring boot project

---
+++ src/main/java/com/furkan/filrouge_api/FilrougeApiApplication.java
@@ -0,0 +1,13 @@
+package com.furkan.filrouge_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilrougeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilrougeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/furkan/filrouge_api/FilrougeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furkan.filrouge_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilrougeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
