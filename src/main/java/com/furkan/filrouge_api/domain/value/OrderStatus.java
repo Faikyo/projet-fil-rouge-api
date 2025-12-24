39addc88d2ffb92100f7d1ccacb055c7b8cff89b
Furkan Demirbas <m.furkan0508@gmail.com>
2025-12-24 17:50:39 +0100

    feat: add domain enums

---
+++ src/main/java/com/furkan/filrouge_api/domain/value/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.furkan.filrouge_api.domain.value;
+
+public enum OrderStatus {
+    CREATED,
+    VALIDATED,
+    PAID,
+    CANCELLED
+}
---
+++ src/main/java/com/furkan/filrouge_api/domain/value/Role.java
@@ -0,0 +1,6 @@
+package com.furkan.filrouge_api.domain.value;
+
+public enum Role {
+    CLIENT,
+    ADMIN
+}
